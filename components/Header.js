485b8ecd50322dab65a9821ef3c35e2bd96df05b
EZZAHIR Taha <110688543+EZZAHIRTAHA@users.noreply.github.com>
2023-02-01 01:36:58 +0100

    Add files via upload

---
+++ components/Header.js
@@ -0,0 +1,16 @@
+import React from "react"
+
+const Header = () => {
+    return (
+        <header className="header">
+            <img 
+                src="https://freesvg.org/img/Troll-Face.png" 
+                className="header--image"
+            />
+            <h2 className="header--title">Meme Generator</h2>
+            <h4 className="header--project">EZZAHIR Taha - Meme Generator</h4>
+        </header>
+    )
+}
+
+export default Header
