0de1ee81ebcb7a13dcfefe90f7770e0ec9fb4506
Ezzahir Taha <taha1.ezzahir@gmail.com>
2023-05-30 00:10:41 +0200

    Added download functionality for memes
    This commit adds the ability to download the generated meme with the caption. When the user clicks the 'Download Meme' button, the current meme, including the caption, is converted to an image using canvas.

--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -1,77 +1,113 @@
-import React from "react"
+import React from "react";
+
+const RANDOM_IMG = "http://i.imgflip.com/1bij.jpg";
+const API_URL = "https://api.imgflip.com/get_memes";
 
 const Meme = () => {
+  const [meme, setMeme] = React.useState({
+    topText: "",
+    bottomText: "",
+    randomImage: RANDOM_IMG
+  });
 
+  const [allMemes, setAllMemes] = React.useState([]);
 
-    const [meme, setMeme] = React.useState({
-        topText: "",
-        bottomText: "",
-        randomImage: "http://i.imgflip.com/1bij.jpg" 
-    })
+  const fetchImg = async () => {
+    const res = await fetch(API_URL);
+    const data = await res.json();
+    setAllMemes(data.data.memes);
+    console.log(data.data.memes);
+  };
 
+  React.useEffect(() => {
+    fetchImg();
+  }, []);
 
-    const [allMemes, setAllMemes] = React.useState([])
-    
-    
-    React.useEffect(() => {
-        async function getMemes() {
-            const res = await fetch("https://api.imgflip.com/get_memes")
-            const data = await res.json()
-            setAllMemes(data.data.memes)
-        }
-        getMemes()
-    }, [])
-    
-    const getMemeImage = () => {
-        const randomNumber = Math.floor(Math.random() * allMemes.length)
-        const url = allMemes[randomNumber].url
-        setMeme(prevMeme => ({
-            ...prevMeme,
-            randomImage: url
-        }))
-    }
-    
-    const handleChange = (event)  => {
-        const {name, value} = event.target
-        setMeme(prevMeme => ({
-            ...prevMeme,
-            [name]: value
-        }))
-    }
-    
-    return (
-        <main>
-            <div className="form">
-                <input 
-                    type="text"
-                    placeholder="Top text"
-                    className="form--input"
-                    name="topText"
-                    value={meme.topText}
-                    onChange={handleChange}
-                />
-                <input 
-                    type="text"
-                    placeholder="Bottom text"
-                    className="form--input"
-                    name="bottomText"
-                    value={meme.bottomText}
-                    onChange={handleChange}
-                />
-                <button 
-                    className="form--button"
-                    onClick={getMemeImage}
-                >
-                    Get a new meme image 🖼
-                </button>
-            </div>
-            <div className="meme">
-                <img src={meme.randomImage} className="meme--image" />
-                <h2 className="meme--text top">{meme.topText}</h2>
-                <h2 className="meme--text bottom">{meme.bottomText}</h2>
-            </div>
-        </main>
-    )
-}
+  const getMemeImage = () => {
+    const randomNumber = Math.floor(Math.random() * allMemes.length);
+    const url = allMemes[randomNumber].url;
+    setMeme(meme => ({...meme,randomImage: url}));
+  };
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setMeme(meme => ({...meme,[name]: value}));
+  };
+
+  const handleDownload = () => {
+    const canvas = document.createElement("canvas");
+    const ctx = canvas.getContext("2d");
+    const image = new Image();
+
+    image.crossOrigin = "anonymous";
+    image.onload = () => {
+      canvas.width = image.width;
+      canvas.height = image.height;
+
+      ctx.drawImage(image, 0, 0);
+      ctx.font = "bold 40px Arial";
+      ctx.fillStyle = "white";
+      ctx.strokeStyle = "black";
+      ctx.lineWidth = 2;
+      ctx.textAlign = "center";
+
+      const topText = meme.topText.toUpperCase();
+      const bottomText = meme.bottomText.toUpperCase();
+
+      const textX = canvas.width / 2;
+      const textY = 50;
+
+      ctx.fillText(topText, textX, textY);
+      ctx.strokeText(topText, textX, textY);
+      ctx.fillText(bottomText, textX, canvas.height - textY);
+      ctx.strokeText(bottomText, textX, canvas.height - textY);
+
+      canvas.toBlob((blob) => {
+        const url = URL.createObjectURL(blob);
+        const link = document.createElement("a");
+        link.href = url;
+        link.download = "meme.png";
+        link.click();
+        URL.revokeObjectURL(url);
+      });
+    };
+
+    image.src = meme.randomImage;
+  };
+
+  return (
+    <main>
+      <div className="form">
+        <input
+          type="text"
+          placeholder="Top text"
+          className="form--input"
+          name="topText"
+          value={meme.topText}
+          onChange={handleChange}
+        />
+        <input
+          type="text"
+          placeholder="Bottom text"
+          className="form--input"
+          name="bottomText"
+          value={meme.bottomText}
+          onChange={handleChange}
+        />
+        <button className="form--button" onClick={getMemeImage}>
+          Get a new meme image 🖼
+        </button>
+        <button className="form--button" onClick={handleDownload}>
+          Download Meme
+        </button>
+      </div>
+      <div className="meme">
+        <img src={meme.randomImage} className="meme--image" alt="Meme" />
+        <h2 className="meme--text top">{meme.topText}</h2>
+        <h2 className="meme--text bottom">{meme.bottomText}</h2>
+      </div>
+    </main>
+  );
+};
 
 export default Meme;
